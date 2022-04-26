772967a91ba06255249742c060d52c4efde96fd8
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-04-25 22:16:11 -0400

    added calendar logo, filled in calenderHeader, fixed getcurrentdayClass on day.js

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,16 +2,16 @@ import dayjs from "dayjs";
 import React from "react";
 
 export default function Day({ day }) {
-  function getcurrentDayClass() {
+  function getCurrentDayClass() {
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
-      ? "bg-blue 600 text-wide rounded-full w-7"
+      ? "bg-blue-600 text-white rounded-full w-7"
       : "";
   }
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
         <p className="text-sm mt-1"> {day.format("ddd").toUpperCase()} </p>
-        <p className={`text-sm p-1 my-1 text-center ${getcurrentDayClass()}`}>
+        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
           {day.format("DD")}
         </p>
       </header>
