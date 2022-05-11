8a5915bbdccd23fae0f0ea221530efd9848a4e9d
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-05-11 17:58:43 -0400

    added header components to small calendar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import CreateEventButton from "./CreateEventButton";
+import SmallCalendar from "./SmallCalendar";
 export default function SideBar() {
   return (
     <aside className="border p-5 w-64">
       <CreateEventButton />
+      <SmallCalendar />
     </aside>
   );
 }
--- src/components/SmallCalendar.js
+++ src/components/SmallCalendar.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import { getMonth } from "../util";
 
 export default function SmallCalendar() {
+  const [currentMonthIndex, setCurrentMonthIndex] = useState(dayjs().month());
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  // local state
+  useEffect(() => {
+    setCurrentMonth(getMonth(currentMonthIndex));
+  }, [currentMonthIndex]);
   return (
-    <div>SmallCalendar</div>
-  )
+    <div className="mt-9">
+      <header className="flex justify-between">
+        <p className="text-gray-500 font-bold">
+          {dayjs(new Date(dayjs().year(), currentMonthIndex)).format(
+            "MMMM YYYY"
+          )}
+        </p>
+        <button>
+            <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_left
+            </span>
+        </button>
+        <button>
+            <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_right
+            </span>
+        </button>
+      </header>
+    </div>
+  );
 }
